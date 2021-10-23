27308a1ed8726c238b996022c96f9a2304ac0df7
firaslaroussi <firas.laroussi1@esprit.tn>
2021-10-24 00:18:09 +0200

    firas laroussi first commin

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -1,8 +1,35 @@
 package tn.esprit.spring.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.repository.EntrepriseRepository;
+
 @Service
 public class EntrepriseServiceImpl implements IEntrepriseService {
 
+	@Autowired
+    EntrepriseRepository entrepriseRepoistory;
+
+	@Override
+	public int ajouterEntreprise(Entreprise entreprise) {
+		entrepriseRepoistory.save(entreprise);
+		return entreprise.getId();
+	}
+
+	@Override
+	public void deleteEntrepriseById(int entrepriseId) {
+		entrepriseRepoistory.delete(entrepriseRepoistory.findById(entrepriseId).get());
+		
+	}
+
+	@Override
+	public Entreprise getEntrepriseById(int entrepriseId) {
+		return entrepriseRepoistory.findById(entrepriseId).get();
+	}
+	
+	
 	
 }
--- src/main/java/tn/esprit/spring/services/IEntrepriseService.java
+++ src/main/java/tn/esprit/spring/services/IEntrepriseService.java
@@ -1,7 +1,13 @@
 package tn.esprit.spring.services;
 
+import java.util.List;
+
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Entreprise;
 
 public interface IEntrepriseService {
 	
-	
+	public int ajouterEntreprise(Entreprise entreprise);
+	public void deleteEntrepriseById(int entrepriseId);
+	public Entreprise getEntrepriseById(int entrepriseId);
 }
