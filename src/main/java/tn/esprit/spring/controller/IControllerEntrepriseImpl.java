08add0438d68551cf8147cbca216851dc5ea52c8
firaslaroussi <firas.laroussi1@esprit.tn>
2021-10-24 15:32:17 +0200

    Entreprise Controller
    seulement les controllers de entreprise , pas departement

--- src/main/java/tn/esprit/spring/controller/IControllerEntrepriseImpl.java
+++ src/main/java/tn/esprit/spring/controller/IControllerEntrepriseImpl.java
@@ -1,6 +1,34 @@
 package tn.esprit.spring.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.services.IEmployeService;
+import tn.esprit.spring.services.IEntrepriseService;
+import tn.esprit.spring.services.ITimesheetService;
 @Controller
 public class IControllerEntrepriseImpl{
+	
+	@Autowired
+	IEmployeService iemployeservice;
+	@Autowired
+	IEntrepriseService ientrepriseservice;
+	@Autowired
+	ITimesheetService itimesheetservice;
+	
+	public int ajouterEntreprise(Entreprise ssiiConsulting) {
+		ientrepriseservice.ajouterEntreprise(ssiiConsulting);
+		return ssiiConsulting.getId();
+	}
+	
+	public void deleteEntrepriseById(int entrepriseId)
+	{
+		ientrepriseservice.deleteEntrepriseById(entrepriseId);
+	}
+	
+	public Entreprise getEntrepriseById(int entrepriseId) {
+
+		return ientrepriseservice.getEntrepriseById(1);
+	}
 
 }
